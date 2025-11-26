6be44e543323b0ed76061ac9f50d5316762e0b7b
Axel Le Bourhis <axel.lebourhis@nxp.com>
2025-11-26 11:04:15 +0100

    [MCSE-1277][mcxw23] Define gRngIsrPrio_c on the preprocessor
    Currently, gRngIsrPrio_c is defined in fwk_config.h but also in RNG_Interface.h if not already defined.
    This makes it cumbersome to manage include order, and often leads to build warnings due to redefinition.
    To avoid that, define the macro on the preprocessor to make it global.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- platform/mcxw23/configs/fwk_config.h
+++ platform/mcxw23/configs/fwk_config.h
@@ -14,14 +14,4 @@
 #define gPlatformFlashStartAddress_c (0)
 #define gPlatformFlashEndAddress_c   (FSL_FEATURE_SYSCON_FLASH_SIZE_BYTES - 1)
 
-/*! *********************************************************************************
- *   RNG Module Configuration
- ********************************************************************************** */
-#if !defined(gSecLibUsePsa_d) || (gSecLibUsePsa_d == 0)
-/*! Set priority of TRNG interrupt (must be lower than link layer interrupts).
- *  See system_MCXW23*.* files for more details on the system requirements regarding interrupt priorities.
- *  Note: When PSA is used, it uses polling to access TRNG, so gRngIsrPrio_c is not needed. */
-#define gRngIsrPrio_c ((NVIC_DEFAULT_PRIORITY) << (8U - (uint8_t)__NVIC_PRIO_BITS))
-#endif
-
 #endif /* _FWK_CONFIG_H_ */
