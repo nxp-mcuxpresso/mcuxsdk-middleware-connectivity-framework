a1e9ca990b7a8eb16d2452f0d5dbc4bf6b23e01d
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-01-12 18:18:31 +0100

    [MCSE-492][wireless_mcu] Exclude board_platform.h when building for Zephyr
    board_platform.h doesn't exist in Zephyr context, we need to exclude it
    from includes.
    Configuration macros can be overriden at preprocessor level if required.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- platform/wireless_mcu/fwk_platform.c
+++ platform/wireless_mcu/fwk_platform.c
@@ -7,8 +7,11 @@
 /*                                  Includes                                  */
 /* -------------------------------------------------------------------------- */
 
+#ifndef __ZEPHYR__
 /* Get some BOARD_***_DEFAULT macro values if defined in board.h for 32kHz settings */
 #include "board_platform.h"
+#endif
+
 #include "fwk_platform_definitions.h"
 #include "fwk_platform.h"
 #include "fwk_config.h"
