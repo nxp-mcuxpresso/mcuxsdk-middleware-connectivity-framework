783b096415bb2e211287109b78d1d1171a564fb7
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-01-19 16:55:13 +0100

    [MCSE-1362][mcxw23] Enable gSecLibUseDspExtension_d
    MCXW23 CM33 core supports DSP extensions, so enable the ultra fast
    library in SecLib.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- platform/mcxw23/configs/fwk_config.h
+++ platform/mcxw23/configs/fwk_config.h
@@ -15,6 +15,18 @@
 #define gPlatformFlashEndAddress_c   (FSL_FEATURE_SYSCON_FLASH_SIZE_BYTES - 1)
 
 /*********************************************************************
+ *        General
+ *********************************************************************/
+
+/*
+ * MCXW23 CM33 core has the DSP extension (__DSP_PRESENT), which allows the use of the NXP UltraFast EC P256
+ * library. The flags may be set to 0 if this library is not required. It mainly makes sense for BLE point validation.
+ */
+#ifndef gSecLibUseDspExtension_d
+#define gSecLibUseDspExtension_d 1
+#endif
+
+/*********************************************************************
  *        PLATFORM
  *********************************************************************/
 /* Enable TimerManager support */
