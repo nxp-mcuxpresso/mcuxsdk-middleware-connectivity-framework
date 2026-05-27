c8358376ff9079cd15e61448fa2ec48ca832f522
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2026-05-27 18:22:16 +0200

    [MCSE-1559][SecLib_RNG] Add support for RNG seed
    - Add WorkQueue-based automatic reseed support (gRngEnableAutoReseed_d)
    - Add NBU seed forwarding via PLATFORM_SendRngSeed (gPlatformHasNbu_d)
    - Register NBU reseed request callback in RNG_Init
    - Add support for RNG_NotifyReseedNeeded, RNG_IsReseedNeeded
    - Add reseed counter logic (gRngMaxRequests_d) in RNG_GetPseudoRandomData
    
    Signed-off-by: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>

--- services/SecLib_RNG/RNG_psa.c
+++ services/SecLib_RNG/RNG_psa.c
@@ -6,7 +6,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  ********************************************************************************** */
 #include "RNG_Interface.h"
+#include "fwk_config.h"
+#include "fwk_platform.h"
 #include "fwk_platform_crypto.h"
+#include "fwk_platform_rng.h"
+#if defined(gPlatformHasNbu_d)
+#include "fwk_platform_ics.h"
+#endif
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+#include "fwk_workq.h"
+#endif
 #include "entropy_poll.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/psa_util.h"
@@ -22,7 +31,9 @@
 
 typedef struct rng_ctx_t
 {
-    bool_t mRngCtxInitialized;
+    bool_t   mRngCtxInitialized;
+    bool_t   mNeedReseed;
+    uint32_t mPRNG_Requests;
 } RNG_context_t;
 
 /*! *********************************************************************************
@@ -33,6 +44,8 @@ typedef struct rng_ctx_t
 
 static RNG_context_t rng_ctx = {
     .mRngCtxInitialized = FALSE,
+    .mNeedReseed        = FALSE,
+    .mPRNG_Requests     = gRngMaxRequests_d,
 };
 
 /*! *********************************************************************************
@@ -46,6 +59,20 @@ static RNG_context_t rng_ctx = {
 * Private prototypes
 *************************************************************************************
 ********************************************************************************** */
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+static void RNG_seed_needed_handler(fwk_work_t *work);
+#endif
+
+/*! *********************************************************************************
+*************************************************************************************
+* Private variables
+*************************************************************************************
+********************************************************************************** */
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+static fwk_work_t seed_needed_work = {
+    .handler = RNG_seed_needed_handler,
+};
+#endif
 
 /*! *********************************************************************************
 *************************************************************************************
@@ -74,17 +101,37 @@ int RNG_Init(void)
             result = gRngSuccess_d;
             break;
         }
+
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+        /* The workqueue is used to post and schedule seed
+         * trig upon user demand using RNG_NotifyReseedNeeded().
+         */
+        if (WORKQ_InitSysWorkQ() < 0)
+        {
+            break;
+        }
+#endif
+
+#if defined(gPlatformHasNbu_d)
+        /* On Host MCU: register callback so NBU can request a reseed */
+        PLATFORM_RegisterReceivedSeedRequest(&RNG_NotifyReseedNeeded);
+#endif
+
         /* initialize psa crypto hardware */
         psa_status_t status = psa_crypto_init();
         if (status != PSA_SUCCESS)
         {
             break;
         }
+
         /* set global variable mRngCtxInitialized */
         rng_ctx.mRngCtxInitialized = TRUE;
 
+        /* Set seed for pseudo random number generation */
+        (void)RNG_SetSeed();
+
         result = gRngSuccess_d;
-    } while (0 != 0);
+    } while (false);
 
     return result;
 }
@@ -116,6 +163,8 @@ void RNG_DeInit(void)
     /* Free PSA crypto resources */
     mbedtls_psa_crypto_free();
     rng_ctx.mRngCtxInitialized = FALSE;
+    rng_ctx.mNeedReseed        = FALSE;
+    rng_ctx.mPRNG_Requests     = gRngMaxRequests_d;
 }
 
 /*! *********************************************************************************
@@ -166,6 +215,7 @@ int RNG_GetTrueRandomNumber(uint32_t *pRandomNo)
  *              The length of the seed if present is 32 bytes.
  *
  * \return  The number of bytes copied OR
+ *          -1 if reseed is needed OR
  *          -3 if the PRNG was not initialized OR
  *          -2 if 0 bytes were requested OR
  *          -1 if an error occurred
@@ -191,6 +241,19 @@ int RNG_GetPseudoRandomData(uint8_t *pOut, uint8_t outBytes, uint8_t *pSeed)
         }
         else
         {
+#if (gRngMaxRequests_d > 0)
+            if (rng_ctx.mPRNG_Requests == gRngMaxRequests_d)
+            {
+                if (RNG_NotifyReseedNeeded() < 0)
+                {
+                    ret = gRngInternalError_d;
+                    break;
+                }
+            }
+            /* Continue in spite of the gRngMaxRequests_d limit reached */
+            rng_ctx.mPRNG_Requests++;
+#endif
+
             if (outBytes > mPRNG_NoOfBytes_c)
             {
                 outBytes = mPRNG_NoOfBytes_c;
@@ -208,30 +271,87 @@ int RNG_GetPseudoRandomData(uint8_t *pOut, uint8_t outBytes, uint8_t *pSeed)
 }
 
 /*! *********************************************************************************
- * \brief  not supported
+ * \brief  Generate a seed and send it to the NBU core if applicable.
+ *         On platforms with NBU, generates entropy via PSA and forwards it.
+ *         On all platforms, clears the reseed flag and resets the request counter.
+ *
+ * \return  gRngSuccess_d on success, gRngInternalError_d on failure.
  *
  ********************************************************************************** */
 int RNG_SetSeed(void)
 {
-    return 0;
+    uint8_t seed[mPRNG_NoOfBytes_c];
+    int     status = gRngInternalError_d;
+
+    do
+    {
+        if (rng_ctx.mRngCtxInitialized != TRUE)
+        {
+            status = gRngNotInitialized_d;
+            break;
+        }
+
+        /* Generate seed entropy using PSA */
+        if (psa_generate_random(seed, mPRNG_NoOfBytes_c) != PSA_SUCCESS)
+        {
+            break;
+        }
+
+#if defined(gPlatformHasNbu_d)
+        /* Forward seed to the NBU via inter-core communication */
+        if (PLATFORM_SendRngSeed(seed, mPRNG_NoOfBytes_c) < 0)
+        {
+            break;
+        }
+#endif
+
+        status = gRngSuccess_d;
+
+        rng_ctx.mNeedReseed = FALSE;
+
+        /* Reset to 1 the request to pseudo random number generation when reseeding */
+        rng_ctx.mPRNG_Requests = 1U;
+    } while (false);
+
+    return status;
 }
 
 /*! *********************************************************************************
- * \brief  not supported
+ * \brief  Notify that a reseed is needed. Sets the reseed flag and optionally
+ *         submits to the WorkQueue for deferred processing.
+ *
+ * \return  gRngSuccess_d on success.
  *
  ********************************************************************************** */
 int RNG_NotifyReseedNeeded(void)
 {
-    return 0;
+    int status = gRngSuccess_d;
+    do
+    {
+        if (rng_ctx.mRngCtxInitialized != TRUE)
+        {
+            status = gRngNotInitialized_d;
+            break;
+        }
+
+        rng_ctx.mNeedReseed = TRUE;
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+        status = WORKQ_Submit(&seed_needed_work);
+#endif
+    } while (false);
+
+    return status;
 }
 
 /*! *********************************************************************************
- * \brief  not supported
+ * \brief  Returns whether reseeding is required or not.
+ *
+ * \return  TRUE if reseed is needed, FALSE otherwise.
  *
  ********************************************************************************** */
 bool_t RNG_IsReseedNeeded(void)
 {
-    return FALSE;
+    return rng_ctx.mNeedReseed;
 }
 
 /*! *********************************************************************************
@@ -240,15 +360,18 @@ bool_t RNG_IsReseedNeeded(void)
  *
  *  \param[in]  external_seed  Pointer to 32 byte array used to set seed.
  *
+ *  \return  gRngSuccess_d on success
+ *           1 if not applicable on this platform.
+ *
  ********************************************************************************** */
 int RNG_SetExternalSeed(uint8_t *external_seed)
 {
     (void)external_seed;
-    return 1; /* External seeding is not supported on psa */
+    return 1; /* External seeding not supported on PSA */
 }
 
 /*! *********************************************************************************
- * \brief  not supported
+ * \brief  not supported - PSA does not expose a PRNG function pointer
  *
  ********************************************************************************** */
 fpRngPrng_t RNG_GetPrngFunc(void)
@@ -257,13 +380,31 @@ fpRngPrng_t RNG_GetPrngFunc(void)
 }
 
 /*! *********************************************************************************
- * \brief  not supported
+ * \brief  not supported - PSA does not expose a PRNG context
  *
  ********************************************************************************** */
 void *RNG_GetPrngContext(void)
 {
     return NULL;
 }
+
+/*! *********************************************************************************
+*************************************************************************************
+* Private functions
+*************************************************************************************
+********************************************************************************** */
+
+#if defined(gRngEnableAutoReseed_d) && (gRngEnableAutoReseed_d > 0)
+static void RNG_seed_needed_handler(fwk_work_t *work)
+{
+    NOT_USED(work);
+    /* Execute reseed request from WorkQ */
+    if (rng_ctx.mNeedReseed == TRUE)
+    {
+        (void)RNG_SetSeed();
+    }
+}
+#endif
 
 #ifdef MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG
 /** External random generator function, implemented by the platform.
