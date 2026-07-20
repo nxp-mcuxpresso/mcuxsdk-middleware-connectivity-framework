c5eaa7a302eec474948f65a3b06eff34fef3c564
Corentin Goubin <corentin.goubin@nxp.com>
2026-07-20 08:06:53 +0000

    [MCSE-1696][ModuleInfo][SCR] Update connectivity framework version from 7.4.1 to 7.4.2

--- services/ModuleInfo/ModuleInfo.c
+++ services/ModuleInfo/ModuleInfo.c
@@ -20,7 +20,7 @@
 /*! \cond DOXY_SKIP_TAG */
 #define gFwkVerMajor_c 7
 #define gFwkVerMinor_c 4
-#define gFwkVerPatch_c 1
+#define gFwkVerPatch_c 2
 #define gFwkBuildNo_c  0
 #define gFwkModuleId_c 0xA3
 /*! \endcond */
